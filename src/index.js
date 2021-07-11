aceaf385311907820366b62e7da1e35707090f80
Nikolay Yaryhin <nik.oroferov@gmail.com>
2021-07-11 20:55:22 +0300

    add promisification

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // import Swal from 'sweetalert2/dist/sweetalert2.js'
 import './js/timer.js';
 import './js/color-switch.js';
+import './js/promisification1.js';
+import './js/promisification2.js';
+import './js/promisification3.js';
 
